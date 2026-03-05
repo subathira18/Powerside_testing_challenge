ef2f34d785dee3f0ccbe085b720320f9a2ab0920
subathira18 <subathira18@gmail.com>
2026-03-05 00:46:01 -0500

    Added cypress config file

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  allowCypressEnv: false,
+
+  e2e: {
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    specPattern: "cypress/e2e/**/*.js",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
